a50ce0127187d30874954be7ab30d56a83afa0a9
ivan <guiteache@gmail.com>
2023-04-14 11:49:25 +0300

    header added components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,22 @@
 function Header() {
   return ( 
-    <header>I`m header</header>
+    <header className="header">
+      <div className="container">
+        <div className="content">
+          <div className="logo"></div>
+          <div className="statistic">
+          <svg className="scheme-icon" version="1.1"
+            baseProfile="full"
+            width="16" height="16"
+            xmlns="http://www.w3.org/2000/svg">
+            <rect className="scheme-icon__rect scheme-icon__rect_first" y="50%" width="4" height="50%" fill="#DC3E22"/>
+            <rect className="scheme-icon__rect scheme-icon__rect_second" x="6" width="4" height="100%" fill="#DC3E22"/>
+            <rect className="scheme-icon__rect scheme-icon__rect_fthird" x="12" y="5" width="4" height="11" fill="#DC3E22"/>
+          </svg>  
+          </div>
+        </div>
+      </div>
+    </header>
    );
 }
 
